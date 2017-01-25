19a6ed219b4275124c85262f7a322a3821a837f7
Artiom Fotescu <artiomfotescu1@gmail.com>
2017-01-25 22:35:38 +0200

    Fix

--- apiFileData.js
+++ apiFileData.js
@@ -20,21 +20,25 @@ export default class ${name}Ctrl {
         const data = await ${moduleName}.getMany();
         ctx.body = response(ctx.method, ctx.lang.code, data.info, data.optional);
     }
+    
     static async getOne(ctx) {
         const ${moduleName} = new Model(ctx.state.user, ctx.params, ctx.query);
         const data = await ${moduleName}.getOne();
         ctx.body = response(ctx.method, ctx.lang.code, data);
     }
+    
     static async post(ctx) {
         const ${moduleName} = new Model(ctx.state.user, ctx.params, ctx.query, ctx.request.body);
         const data = await ${moduleName}.post();
         ctx.body = response(ctx.method, ctx.lang.code, data);
     }
+    
     static async put(ctx) {
         const ${moduleName} = new Model(ctx.state.user, ctx.params, ctx.query, ctx.request.body);
         const data = await ${moduleName}.put();
         ctx.body = response(ctx.method, ctx.lang.code, data);
     }
+    
     static async delete(ctx) {
         const ${moduleName} = new Model(ctx.state.user, ctx.params, ctx.query);
         await ${moduleName}.delete();
@@ -52,39 +56,43 @@ import { sql, qFile, qPath, additionalQuery } from './../../../shared/database';
 const sqlDirPath = path.join(__dirname, './sql');
 
 export default class ${name}Ctrl {
+
     constructor(user, params, qs, body = {}) {
         this.user = user;
         this.params = params;
         this.qs = qs;
         this.body = body;
     }
+    
     async getMany() {
         let sqlQuery = qFile(qPath(sqlDirPath, 'getMany')).query;
         const countQuery = qFile(qPath(sqlDirPath, 'getTotalCount')).query;
         const data = { info: null, optional: { limit: this.qs.limit || 50, offset: this.qs.offset || 0 } };
+        
         sqlQuery += additionalQuery(this.qs);
         await sql.task(async (task) => {
             const total = await task.one(countQuery);
             data.optional.total = total.count;
             data.info = await task.any(sqlQuery);
         });
+        
         return data;
     }
+    
     async getOne() {
-        const sqlQuery = qFile(qPath(sqlDirPath, 'getOne')).query;
-        return sql.one(sqlQuery, this.params);
+        return sql.one(qFile(qPath(sqlDirPath, 'getOne')), this.params);
     }
+    
     async post() {
-        const sqlQuery = qFile(qPath(sqlDirPath, 'post')).query;
-        return sql.one(sqlQuery, { ...this.params, ...this.body });
+        return sql.one(qFile(qPath(sqlDirPath, 'post')), { ...this.params, ...this.body });
     }
+    
     async put() {
-        const sqlQuery = qFile(qPath(sqlDirPath, 'put')).query;
-        return sql.one(sqlQuery, { ...this.params, ...this.body });
+        return sql.one(qFile(qPath(sqlDirPath, 'put')), { ...this.params, ...this.body });
     }
+    
     async delete() {
-        const sqlQuery = qFile(qPath(sqlDirPath, 'delete')).query;
-        return sql.none(sqlQuery, this.params);
+        return sql.none(qFile(qPath(sqlDirPath, 'delete')), this.params);
     }
 }
 `
